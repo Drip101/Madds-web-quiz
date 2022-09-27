4e82538a8d2e372553d4de574c486319544bdded
Maddrick Long <110777599+Drip101@users.noreply.github.com>
2022-09-26 22:23:55 -0400

    add questions

--- script.js
+++ script.js
@@ -9,15 +9,27 @@ var Questions = [
         answer: 'c. Both the <head> section and the <body> section are correct.'
     }
     , {
-        question: 'What is 2 + 2',
-        choices: 1,
-        answer: 1
+        question: 'Question 2: "How can you add a comment in JavaScript?',
+        choices: ['a. <!--This is a comment-->', 'b. //This is a comment', 'c.This is a comment', 'd. This is a comment'],
+        answer: 'b. //This is a comment'
     }
-    // {
-    //         question:
-    //             choices:
-    // answer:
-    // }
+    , {
+        question: 'Question 3: What is the correct way to write a JavaScript array?',
+        choices: ['a. var colors = ["red", "green", "blue"]', 'b. var colors = ("red"), 2 =("green"), 3 =("blue")', 'c. var colors = "red", "green", "blue"', 'd. var colors = (1:"red", 2:"green", 3:"blue")'],
+        answer: 'a. var colors = ["red", "green", "blue"]'
+    }
+
+    , {
+        question: 'Question 4: What does HTML stand for?',
+        choices: ['a. Home Tool Markup Language', 'b. Hyper Text Markup Language', 'c. Hyperlinks and Text Markup Language', 'd. None of the above'],
+        answer: 'b. Hyper Text Markup Language'
+    }
+
+    , {
+        question: 'Question 5: What does CSS stand for?',
+        choices: ['a. Computer Style Sheets', 'b. Creative Style Sheets', 'c. Cascading Style Sheets', 'd. Colorful Style Sheets']
+        answer: 'c. Cascading Style Sheets'
+    }
 ]
 startbutton.addEventListener('click', function () {
     startGame()
